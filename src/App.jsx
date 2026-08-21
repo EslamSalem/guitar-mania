458c5bb46cf6571dc080e9a5a29d1c9c914c78a0
EslamSalem <eslamsalem2.es@gmail.com>
2026-08-21 19:01:47 +0300

    Added Header Placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  
   return (
-    <div>
-      <h1>Guitar Mania</h1>
-    </div>
-  )
+    <>
+      <Header />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import "../styles/Header.css"
+
+function Header() {
+  return (
+    <header>
+      <h1 id="main-logo">Guitar Mania</h1>
+      <nav>
+        <li>Link 1</li>
+        <li>Link 2</li>
+        <li>Link 3</li>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
