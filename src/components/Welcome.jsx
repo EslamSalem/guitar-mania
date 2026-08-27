1161776e15eb20f58960314593da544590af1443
EslamSalem <eslamsalem2.es@gmail.com>
2026-08-27 17:30:56 +0300

    Update Welcome Section

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,21 @@
-import listImg from "../assets/list-temp.jpg"
+import listImg from "../assets/list-temp.jpg";
 import Card from "./Card";
-import "../styles/Welcome.css"
+import "../styles/Welcome.css";
 
 function Welcome() {
   return (
     <section id="welcome">
-      <h1>Welcome to Guitar Mania!</h1>
-      <h2>For everything guitars.</h2>
+      <div id="welcome-msg">
+        <h1>Welcome to Guitar Mania!</h1>
+        <h2>For Everything Guitars</h2>
+      </div>
+      <div id="welcome-text">
+        <p>
+          Here, you can learn cool facts about your favorite rock & metal
+          guitarists, learn music theory, and much more.
+        </p>
+        <p>Ready to dive in to the music?</p>
+      </div>
       <div id="features">
         <Card id="list-card" img={listImg} title="Famous Rockers" />
         <Card id="list-card" img={listImg} title="Learn Theory" />
