651159fc3cf324695eea9324227a1301fb88dd55
EslamSalem <eslamsalem2.es@gmail.com>
2026-08-27 17:31:41 +0300

    Add Card Hover Animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,9 @@ import "../styles/Card.css";
 function Card({ id, img, title }) {
   return (
     <div id={id} className="card">
-      <img src={img} alt="Card Background Image" />
+      <div className="card-img-wrapper">
+        <img src={img} alt="Card Background Image" />
+      </div>
       <h3 className="card-title">{title}</h3>
     </div>
   );
