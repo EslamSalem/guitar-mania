af30230ebec13c60b28bbfd881323dabe8d453aa
EslamSalem <eslamsalem2.es@gmail.com>
2026-09-01 10:22:51 +0300

    Rename Hero Component

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,7 +1,7 @@
 import heroLogo from "../assets/hero/hero-logo-2.png";
 import "../styles/Landing.css"
 
-function Hero() {
+function Landing() {
   return (
     <section id="hero">
       <img src={heroLogo} alt="Guitar Mania Logo" />
@@ -9,4 +9,4 @@ function Hero() {
   )
 }
 
-export default Hero;
+export default Landing;
