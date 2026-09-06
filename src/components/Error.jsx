5cfce49c81f410d305857edb4df59bea20a2423c
EslamSalem <eslamsalem2.es@gmail.com>
2026-09-06 12:44:27 +0300

    Generalize Error Page

---
+++ src/components/Error.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router";
+import Header from "./Header";
+import Footer from "./Footer";
+import "../styles/Error.css";
+import "../styles/Hero.css";
+
+function Error({ status }) {
+  return (
+    <div className="error-page">
+      <Header />
+      <div className="body-msg">
+        <h1 className="bosy-msg-title">
+          {status === 404 ? "Page Not Found!" : "Internal Server Error!"}
+        </h1>
+        <Link to={"/"} className="hero-btn">
+          Go Back Home
+        </Link>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Error;
