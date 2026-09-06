aa84a3588e35d65a28a456e715607f530636ccb2
EslamSalem <eslamsalem2.es@gmail.com>
2026-09-06 18:30:49 +0300

    Add Rockers Page

--- src/components/Rockers.jsx
+++ src/components/Rockers.jsx
@@ -1,13 +1,21 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import "../styles/Rockers-Page.css";
 
 function Rockers() {
   return (
     <section id="rockers-page">
       <Header />
+      <main id="rockers-page-content">
+        <div id="rockers-filter"></div>
+        <div id="rockers-container">
+          <h1 id="rockers-title">Famous Rockers</h1>
+          <div id="rockers-cards"></div>
+        </div>
+      </main>
       <Footer />
     </section>
-  )
+  );
 }
 
 export default Rockers;
