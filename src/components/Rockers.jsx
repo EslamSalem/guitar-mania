34df67b730a3e20b2f1ea0b373f2f00b75419a41
EslamSalem <eslamsalem2.es@gmail.com>
2026-09-17 19:03:22 +0300

    Split Rockers Data Images

--- src/components/Rockers.jsx
+++ src/components/Rockers.jsx
@@ -62,7 +62,7 @@ function Rockers() {
           ) : rockersSorted.length > 0 ? (
             <div id="rockers-cards">
               {rockersSorted.map((item) => (
-                <Card id={crypto.randomUUID()} img={item.imgURL} title={item.name} />
+                <Card id={crypto.randomUUID()} img={item.cardImgURL} title={item.name} />
               ))}
             </div>
           ) : (
